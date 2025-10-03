60744df2955fc8a81de780a47aac168a3bc70ae5
Sofienholm <eaa24snho@students.eaaa.dk>
2025-10-03 10:42:43 +0200

    overskriftændring

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
   return (
     <section className="page">
-      <h1>123test Page</h1>
+      <h1>About Page</h1>
       <p>Who we are and what we do 🤷🏼‍♂️</p>
     </section>
   );
