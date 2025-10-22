b66508dd135c67f4c5a23c9c8544ee80745c0a71
Martinlpilgaard <eaa24mlpk@students.eaaa.dk>
2025-10-22 15:40:04 +0200

    fejl

--- src/components/layout/PageHeader.jsx
+++ src/components/layout/PageHeader.jsx
@@ -1,26 +1,37 @@
 import React from "react";
 import styles from "./PageHeader.module.css";
 
-
+// Ikoner fra public/
 import searchIcon from "../../../public/assets/icon/ic-search-symbol.svg";
+import profileIcon from "../../../public/assets/illustrations/ill-profil-avatar-man-garlic.svg";
 
 export default function PageHeader() {
   return (
     <header className={styles.header}>
-      <div className={styles.searchWrap}>
-        <input
-          id="recipe-search"
-          type="search"
-          placeholder="Find en af dine opskrifter"
-          className={styles.searchInput}
-        />
-        {/* Lup-ikon (SVG fra public/) */}
-        <img
-          src={searchIcon}
-          alt=""
-          aria-hidden="true"
-          className={styles.searchIcon}
-        />
+      <div className={styles.inner}>
+        <div className={styles.searchWrap}>
+          <input
+            id="recipe-search"
+            type="search"
+            placeholder="Find en af dine opskrifter"
+            className={styles.searchInput}
+          />
+          <img
+            src={searchIcon}
+            alt=""
+            aria-hidden="true"
+            className={styles.searchIcon}
+          />
+        </div>
+
+        <button className={styles.profileButton} aria-label="Gå til profil">
+          <img
+            src={profileIcon}
+            alt=""
+            aria-hidden="true"
+            className={styles.profileIcon}
+          />
+        </button>
       </div>
     </header>
   );
