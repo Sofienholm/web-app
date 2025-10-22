1b5ad3654d2cdd9beccbf58b6c2e32ae14a3a4fc
Martinlpilgaard <eaa24mlpk@students.eaaa.dk>
2025-10-22 14:38:25 +0200

    søgefeldtet

--- src/components/layout/PageHeader.jsx
+++ src/components/layout/PageHeader.jsx
@@ -1,6 +1,27 @@
+import React from "react";
+import styles from "./PageHeader.module.css";
+
+
+import searchIcon from "../../../public/assets/icon/ic-search-symbol.svg";
+
 export default function PageHeader() {
   return (
-    <header className="page-header">
+    <header className={styles.header}>
+      <div className={styles.searchWrap}>
+        <input
+          id="recipe-search"
+          type="search"
+          placeholder="Find en af dine opskrifter"
+          className={styles.searchInput}
+        />
+        {/* Lup-ikon (SVG fra public/) */}
+        <img
+          src={searchIcon}
+          alt=""
+          aria-hidden="true"
+          className={styles.searchIcon}
+        />
+      </div>
     </header>
   );
 }
