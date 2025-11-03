0a904b6cacc785219a0f547daa7dc739f9fbc92a
Sofienholm <eaa24snho@students.eaaa.dk>
2025-11-03 10:36:54 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: "/"
+    base: "/web-app/"
   };
 
   // Change base path when building for production
